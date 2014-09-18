f528f36f77e60dbccc7bbb8fb5f9ca5544b77769
Timothy Pfafman <tim@pfafman.com>
2014-09-18 14:42:32 -0700

    update for meteor 0.9

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-    summary: "Facebook SDK packaged for Meteor"
+  summary: "Facebook SDK packaged for Meteor",
+  version: "0.1.0",
+  git: "https://github.com/biasport/facebook-sdk.git"
 });
 
 Package.on_use(function(api) {
-    api.use(['jquery', 'spacebars', 'ui', 'templating', 'meteor', 'mongo-livedata', 'livedata', 'webapp']);
+  api.versionsFrom("METEOR@0.9.0");
+    api.use(['jquery', 'spacebars', 'ui', 'templating']);
     api.add_files(['sdk.js', 'templates.html'], 'client');
 });
