de0e4f42bbb8825b895c87887d52f6eea5cdae55
Timothy Pfafman <tim@pfafman.com>
2014-10-29 06:31:56 -0700

    version bump

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Facebook SDK packaged for Meteor",
-  version: "0.1.0",
+  version: "0.2.0",
   git: "https://github.com/biasport/facebook-sdk.git"
 });
 
 Package.on_use(function(api) {
   api.versionsFrom("METEOR@0.9.0");
-    api.use(['jquery', 'spacebars', 'ui', 'templating']);
+    api.use(['jquery', 'spacebars', 'templating']);
     api.add_files(['sdk.js', 'templates.html'], 'client');
 });
